48b9deadb0555bdaa7902ac23d09cc3b44c2bcdb
ChristopherZinger <krzysztof.zinger@gmail.com>
2020-09-05 21:01:39 +0200

    add todo routes

---
+++ backend/todos/routes/TodoRoutes.js
@@ -0,0 +1,13 @@
+const { Router } = require('express');
+const controllers = require('../controllers/TodoController');
+
+
+const router = Router();
+
+
+
+// router.post('/login', controllers.login);
+// router.post('/signup', controllers.signup);
+// router.post('/logout', controllers.logout);
+
+module.exports = router;
