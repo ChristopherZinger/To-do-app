0b84b256e74f9f400f4a9158a98e4f5fc097ca4f
ChristopherZinger <krzysztof.zinger@gmail.com>
2020-09-30 13:01:17 +0200

    frontend - adjust Signup component to new AuthForm fromat

--- frontend/app/src/components/auth/Signup/Signup.js
+++ frontend/app/src/components/auth/Signup/Signup.js
@@ -1,153 +1,84 @@
 
-import React, { useState } from 'react';
-import axios from 'axios';
-import * as EmailValidator from "email-validator";
+import React, { Fragment, useState } from 'react';
+import AuthForm from '../AuthForm/AuthForm';
 import { auth } from '../../../utils/auth/auth';
 
-const Signup = (props) => {
-    // state
-    const [passwordConfirmErrors, setPasswordConfirmErrors] = useState('');
-    const [passwordErrors, setPasswordErrors] = useState('');
-    const [emailErrors, setEmailErrors] = useState('');
-    const [signupBtnIsActive, setSignupBtnIsActive] = useState(true);
+export default class SignupForm extends React.Component {
+    render() {
+        const inputs = []
 
-    function handleErrors(errors, inputs) {
-        if (errors) {
-            // password errors - backend errors
-            if (errors.password !== undefined) {
-                setPasswordErrors(errors.password)
-                return;
+        // add email field
+        inputs.push({
+            input: {
+                id: "email",
+                name: "email",
+                type: "email",
+                placeholder: "Email",
+                classes: [],
+            },
+            label: {
+                txt: "Email"
+            },
+            info: {
+                classes: []
             }
+        })
 
-            // email password - backend errors
-            if (typeof errors.email !== undefined) {
-                setEmailErrors(errors.email)
-                return;
+        // add password field
+        inputs.push({
+            input: {
+                id: "password",
+                name: "password",
+                type: "password",
+                placeholder: "Password",
+                classes: [],
+            },
+            label: {
+                txt: "Password"
+            },
+            info: {
+                classes: []
+            }
+        })
+
+        // add password confirmation field
+        inputs.push({
+            input: {
+                id: "passwordConfirm",
+                name: "passwordConfirm",
+                type: "password",
+                placeholder: "Confirm Password",
+                classes: [],
+            },
+            label: {
+                txt: "Confirm Password"
+            },
+            info: {
+                classes: []
+            }
+        })
+
+        const submit = {
+            txt: 'submit',
+            url: '/login',
+            success: (data) => {
+                // save accessToken as variable
+                const { accessToken, expirationPeriod } = data;
+                auth.login(accessToken, expirationPeriod); // from utils/auth/auth
+            },
+            error: () => {
+                console.log('error while submiting login form')
             }
         }
 
-        if (inputs) {
-            // validate email on frontend
-            if (!EmailValidator.validate(inputs.email)) {
-                setEmailErrors("This email is invalid.");
-                return false;
-            };
-
-            // validate password - length
-            if (typeof inputs.password !== 'string' || inputs.password.length < 4) {
-                console.log('too short')
-                setPasswordErrors('This password is too short. It has to include more than 4 characters.');
-                return false;
-            }
-
-            // validate password - repeated correctly
-            if (inputs.password !== inputs.passwordConfirm) {
-                setPasswordConfirmErrors("Type the same password twice.");
-                return false;
-            }
-            return true; // inputs are valid
-        }
+        return (
+            <AuthForm
+                submit={submit}
+                inputs={inputs}
+            >
+                <h4>Signup</h4>
+                <p>Welcome to Todo app. Create new accout with your email.</p>
+            </AuthForm>
+        )
     }
-
-    function resetErrors() {
-        setEmailErrors('');
-        setPasswordErrors('');
-        setPasswordConfirmErrors('');
-    }
-
-    function handleSignupSubmit(e) {
-        if (!signupBtnIsActive) return;
-        setSignupBtnIsActive(false);
-
-        // stop submit
-        e.preventDefault();
-
-        // reset error messages
-        resetErrors()
-
-        // get values from input
-        const email = e.target.email.value;
-        const password = e.target.password.value;
-        const passwordConfirm = e.target.passwordConfirm.value;
-
-        // handle wrong inputs
-        const inputsAreValid = handleErrors(undefined, { email, password, passwordConfirm })
-        if (!inputsAreValid) return;
-
-        // setup data and headers for axios call
-        const data = {
-            "email": email,
-            "password": password
-        }
-
-        // call api
-        axios.post('/signup', data)
-            .then(res => {
-
-                const { accessToken, expirationPeriod } = res.data.auth;
-                auth.login(accessToken, expirationPeriod);
-
-                //update style
-                setSignupBtnIsActive(true);
-
-                //redirect
-                props.history.push({ pathname: "/" }) // redirect to home
-                return console.log('Sign up success : ');
-
-            })
-            .catch(err => {
-                //update style
-                setSignupBtnIsActive(true);
-                const errors = err.response.data.errors;
-                handleErrors(errors);
-            })
-    };
-
-    return (
-        <div>
-            <h4>Signup</h4>
-            <form onSubmit={handleSignupSubmit}>
-                <label htmlFor="emailInput">email</label>
-                <input
-                    className="form-control"
-                    type="text" name="email"
-                    id="emailInput" />
-                <small id="emailHelp" className="form-text text-muted">
-                    {emailErrors ? emailErrors : ''}
-                </small>
-
-                <label htmlFor="password">Password</label>
-                <input
-                    className="form-control"
-                    type="password"
-                    name="password"
-                    id="password" />
-                <small id="emailHelp" className="form-text text-muted">
-                    {passwordErrors ? passwordErrors : ''}
-                </small>
-
-                <label htmlFor="passwordConfirm">Confirm Password</label>
-                <input
-                    className="form-control"
-                    type="password"
-                    name="passwordConfirm"
-                    id="passwordConfirm" />
-                <small id="emailHelp" className="form-text text-muted">
-                    {passwordConfirmErrors ? passwordConfirmErrors : ''}
-                </small>
-                <br />
-
-                <button
-                    type="submit"
-                    id="signupSubmitBtn"
-                    className="btn btn-primary">
-                    Sign up
-                </button>
-
-            </form>
-        </div>
-    )
-};
-
-
-export default Signup;
+}
